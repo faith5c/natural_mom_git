b09ebe18132715d48f099d789b8ae60eafed0e99
hook1028 <hook1028@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-03 12:30:50 +0000

    [소라] view all_sells 컬럼명 수정
    git-svn-id: https://dev.naver.com/svn/naturalmom@252 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/_sora/sql/v_product_deleted.sql
+++ trunk/_sora/sql/v_product_deleted.sql
@@ -6,7 +6,7 @@ SELECT "p"."product_no", "p"."product_name", "p"."represent_img", "p"."selling_p
 "p"."cost_price", "p"."stock", 
 NVL((SELECT SUM("o"."buy_num") FROM "tb_product_n_order" "o" WHERE "o"."product_no" = 
 "p"."product_no" AND "o"."process_cd" = 21
-GROUP BY "o"."product_no"), 0) AS all_sells
+GROUP BY "o"."product_no"), 0) AS "all_sells"
 FROM "tb_product" "p"
 WHERE "p"."deleted_state" = 1;
 
@@ -20,5 +20,5 @@ SELECT * FROM "V_PRODUCT_DELETED" ORDER BY "selling_price", "product_no";
 SELECT * FROM "V_PRODUCT_DELETED" ORDER BY "selling_price" desc, "product_no";
 SELECT * FROM "V_PRODUCT_DELETED" ORDER BY "stock", "product_no";
 SELECT * FROM "V_PRODUCT_DELETED" ORDER BY "stock" desc, "product_no";
-SELECT * FROM "V_PRODUCT_DELETED" ORDER BY all_sells, "product_no";
-SELECT * FROM "V_PRODUCT_DELETED" ORDER BY all_sells desc, "product_no";
+SELECT * FROM "V_PRODUCT_DELETED" ORDER BY "all_sells", "product_no";
+SELECT * FROM "V_PRODUCT_DELETED" ORDER BY "all_sells" desc, "product_no";
--- trunk/_sora/sql/v_product_manage.sql
+++ trunk/_sora/sql/v_product_manage.sql
@@ -7,7 +7,7 @@ SELECT "p"."product_no", "c"."category_nm", "p"."product_name",
 "p"."stock", "p"."display_state", "p"."sale_state",  
 NVL((SELECT SUM("o"."buy_num") FROM "tb_product_n_order" "o" WHERE "o"."product_no" = 
 "p"."product_no" AND "o"."process_cd" = 21
-GROUP BY "o"."product_no"), 0) AS all_sells
+GROUP BY "o"."product_no"), 0) AS "all_sells"
 FROM "tb_category" "c","tb_product" "p", "tb_product_n_order" "o"
 WHERE "c"."category_cd" = "p"."category_cd"
 AND "p"."product_no" = "o"."product_no"(+)
@@ -31,6 +31,6 @@ SELECT * FROM "V_PRODUCT_MANAGE" ORDER BY "display_state", "product_no";
 SELECT * FROM "V_PRODUCT_MANAGE" ORDER BY "display_state" desc, "product_no";
 SELECT * FROM "V_PRODUCT_MANAGE" ORDER BY "sale_state", "product_no";
 SELECT * FROM "V_PRODUCT_MANAGE" ORDER BY "sale_state" desc, "product_no";
-SELECT * FROM "V_PRODUCT_MANAGE" ORDER BY all_sells, "product_no";
-SELECT * FROM "V_PRODUCT_MANAGE" ORDER BY all_sells desc, "product_no";
+SELECT * FROM "V_PRODUCT_MANAGE" ORDER BY "all_sells", "product_no";
+SELECT * FROM "V_PRODUCT_MANAGE" ORDER BY "all_sells" desc, "product_no";
 
