9156b1c06d185c1666a88a34a60bc20ce2b7e60b
sseun0402 <sseun0402@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-05 16:18:10 +0000

    [성은] MemberDaoOraImple
    git-svn-id: https://dev.naver.com/svn/naturalmom@300 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/_sungeun/src/naturalmom/data/dao/impl/MemberDaoOraImple.java
+++ trunk/_sungeun/src/naturalmom/data/dao/impl/MemberDaoOraImple.java
@@ -13,6 +13,7 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+
 import naturalmom.data.dao.IMemberDao;
 import naturalmom.data.model.MemberVo;
 
@@ -29,9 +30,13 @@ public class MemberDaoOraImple extends NamedParameterJdbcDaoSupport implements I
 	private final String SQL_MEMBER_GET_ID ="select mem_id from tb_member where mem_id=?";
 	private final String SQL_MEMBER_INSERT 
 		="insert into tb_member values (:mem_id, :mem_pw, :mem_name, :mem_phone, :mem_addr_post, :mem_addr_detail, :mem_email, :mem_birth, :mem_gender, 0, 1)";
-
-	
-	
+	private final String SQL_MEMBER_UPDATE_LEVEL_CD ="UPDATE NMDB.tb_member SET mem_level_cd=? WHERE mem_id=?";
+	private final String SQL_MEMBER_UPDATE_DROP_OUT = "UPDATE NMDB.tb_member SET drop_out = 1 WHERE mem_id=?";
+	private final String SQL_MEMBER_UPDATE_INFO 
+		="UPDATE NMDB.tb_member SET "
+				+ "mem_pw=:mem_pw, mem_phone=:mem_phone, mem_addr_post=:mem_addr_post, mem_addr_detail=:mem_addr_detail, mem_email=:mem_email "
+				+ "WHERE mem_id=:mem_id";
+	private final String SQL_MEMBER_SELECT_ALL ="SELECT * FROM TB_MEMBER";
 	// Constructor
 	public MemberDaoOraImple() {
 
@@ -39,7 +44,7 @@ public class MemberDaoOraImple extends NamedParameterJdbcDaoSupport implements I
 	
 
 	//Overriding Method
-	@Override		// 비밀번호 찾기, 로그인 체크
+	@Override		// 비밀번호 찾기, 로그인 체크, 회원정보 수정 불러오기
 	public MemberVo getOneMember(String id) {
 		jtem = getJdbcTemplate();
 		List<MemberVo> member_list = jtem.query(SQL_MEMBER_GET_ONE_BY_ID, 
@@ -137,28 +142,53 @@ public boolean checkId(String id) {
 		return nameTemplate.update(SQL_MEMBER_INSERT, ps);
 	}
 
-	@Override
+	
+	
+	@Override		// 회원등급 변경 (관리자:10/일반회원:1/불량회원:2)
 	public int editMemberLevel(String id, int level) {
-		// TODO Auto-generated method stub
-		return 0;
+		jtem = getJdbcTemplate();
+		
+		return jtem.update(SQL_MEMBER_UPDATE_LEVEL_CD, 
+				new Object[]{ new Integer(level), id }, 
+				new int[]{Types.INTEGER, Types.VARCHAR});
 	}
 
-	@Override
+	
+	
+	@Override		// 회원 탈퇴 (사용자, 관리자)
 	public int removeMember(String id) {
-		// TODO Auto-generated method stub
-		return 0;
+		jtem = getJdbcTemplate();
+		
+		return jtem.update(SQL_MEMBER_UPDATE_DROP_OUT, 
+				id, 	Types.VARCHAR );
 	}
 
+	
+	
 	@Override
 	public int editMember(MemberVo member) {
-		// TODO Auto-generated method stub
-		return 0;
+		nameTemplate = getNamedParameterJdbcTemplate();
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+
+		ps.addValue("mem_pw", member.getMem_pw(), Types.VARCHAR);
+		ps.addValue("mem_phone", member.getMem_phone(), Types.VARCHAR);
+		ps.addValue("mem_addr_post", member.getMem_addr_post(), Types.VARCHAR);
+		ps.addValue("mem_addr_detail", member.getMem_addr_detail(), Types.VARCHAR);
+		ps.addValue("mem_email", member.getMem_email(), Types.VARCHAR);
+		ps.addValue("mem_id", member.getMem_id(), Types.VARCHAR);
+		
+		return nameTemplate.update(SQL_MEMBER_UPDATE_INFO, ps);
 	}
 
 	@Override
 	public List<MemberVo> getAllMember() {
-		// TODO Auto-generated method stub
-		return null;
+		jtem =getJdbcTemplate();
+		List<MemberVo> member_list = null;
+		
+		
+		
+		return member_list;
+
 	}
 
 	@Override
@@ -167,5 +197,8 @@ public boolean checkId(String id) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	
+	
 
 }
