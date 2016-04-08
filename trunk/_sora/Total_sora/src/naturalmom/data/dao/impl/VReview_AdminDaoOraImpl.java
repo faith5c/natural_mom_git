568c377e688510adc4f25a29148ea038c0a576d3
hook1028 <hook1028@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-08 00:40:37 +0000

    [소라] 페이지처리
    git-svn-id: https://dev.naver.com/svn/naturalmom@339 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/_sora/Total_sora/src/naturalmom/data/dao/impl/VReview_AdminDaoOraImpl.java
+++ trunk/_sora/Total_sora/src/naturalmom/data/dao/impl/VReview_AdminDaoOraImpl.java
@@ -1,5 +1,6 @@
 package naturalmom.data.dao.impl;
 
+import java.sql.Types;
 import java.util.List;
 
 import naturalmom.data.dao.IVReview_AdminDao;
@@ -7,25 +8,27 @@ import naturalmom.data.model.VReview_AdminVo;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
-public class VReview_AdminDaoOraImpl extends JdbcDaoSupport implements IVReview_AdminDao 
+public class VReview_AdminDaoOraImpl extends NamedParameterJdbcDaoSupport implements IVReview_AdminDao 
 {
 	// 글 개수 구하기
 	private final String GET_COUNT_REVIEWS = "SELECT COUNT(review_no) FROM v_review_admin";		
 	// 게시판 목록 조회
-	private final String GET_ALL_LIST = "SELECT * FROM v_review_admin";
+	private final String GET_ALL_LIST = "SELECT * FROM v_review_admin WHERE ROWNUM >= :start AND ROWNUM <= :end";
 	// 게시판 내용 조회
 	private final String GET_ONE_REVIEW = "SELECT * FROM v_review_admin WHERE review_no = ?";
 	// 제목으로 검색하기
-	private final String GET_SEARCH_BY_TITLE = "SELECT * FROM v_review_admin WHERE rvw_title LIKE ?";
+	private final String GET_SEARCH_BY_TITLE = "SELECT * FROM v_review_admin WHERE rvw_title LIKE :search AND ROWNUM >= :start AND ROWNUM <= :end";
 	// 내용으로 검색하기
-	private final String GET_SEARCH_BY_CONTENT = "SELECT * FROM v_review_admin WHERE rvw_content LIKE ?";
+	private final String GET_SEARCH_BY_CONTENT = "SELECT * FROM v_review_admin WHERE rvw_content LIKE :search AND ROWNUM >= :start AND ROWNUM <= :end";
 	// 글쓴이로 검색하기
-	private final String GET_SEARCH_BY_ID = "SELECT * FROM v_review_admin WHERE mem_id LIKE ?";
+	private final String GET_SEARCH_BY_ID = "SELECT * FROM v_review_admin WHERE mem_id LIKE :search AND  ROWNUM >= :start AND ROWNUM <= :end";
 	// 제목+내용으로 검색하기
 	private final String GET_SEARCH_BY_TITLE_CONTENT = "SELECT * FROM v_review_admin "
-			+ "WHERE (rvw_title LIKE ? OR rvw_content LIKE ?)";
+			+ "WHERE (rvw_title LIKE :search OR rvw_content LIKE :search) AND  ROWNUM >= :start AND ROWNUM <= :end";
 	@Override
 	public List<VReview_AdminVo> getAllList() throws DataAccessException 
 	{
@@ -48,31 +51,55 @@ public class VReview_AdminDaoOraImpl extends JdbcDaoSupport implements IVReview_
 	public List<VReview_AdminVo> getReviews_by_title(String search, int start, int end) throws DataAccessException 
 	{
 		search = "%" + search + "%";
-		return getJdbcTemplate().query(GET_SEARCH_BY_TITLE, 
-				new BeanPropertyRowMapper<VReview_AdminVo>(VReview_AdminVo.class), search);
+		
+		MapSqlParameterSource msps = new MapSqlParameterSource();
+		msps.addValue("search", search, Types.VARCHAR);
+		msps.addValue("start", start, Types.VARCHAR);
+		msps.addValue("end", end, Types.VARCHAR);
+		
+		return getNamedParameterJdbcTemplate().query(GET_SEARCH_BY_TITLE, msps,
+				new BeanPropertyRowMapper<VReview_AdminVo>(VReview_AdminVo.class));
 	}
 
 	@Override
 	public List<VReview_AdminVo> getReviews_by_content(String search, int start, int end) throws DataAccessException 
 	{
 		search = "%" + search + "%";
-		return getJdbcTemplate().query(GET_SEARCH_BY_CONTENT, 
-				new BeanPropertyRowMapper<VReview_AdminVo>(VReview_AdminVo.class), search);
+		
+		MapSqlParameterSource msps = new MapSqlParameterSource();
+		msps.addValue("search", search, Types.VARCHAR);
+		msps.addValue("start", start, Types.VARCHAR);
+		msps.addValue("end", end, Types.VARCHAR);
+		
+		return getNamedParameterJdbcTemplate().query(GET_SEARCH_BY_CONTENT, msps,
+				new BeanPropertyRowMapper<VReview_AdminVo>(VReview_AdminVo.class));
 	}
 
 	@Override
 	public List<VReview_AdminVo> getReviews_by_id(String search, int start, int end) throws DataAccessException 
 	{
 		search = "%" + search + "%";
-		return getJdbcTemplate().query(GET_SEARCH_BY_ID, 
-				new BeanPropertyRowMapper<VReview_AdminVo>(VReview_AdminVo.class), search);
+		
+		MapSqlParameterSource msps = new MapSqlParameterSource();
+		msps.addValue("search", search, Types.VARCHAR);
+		msps.addValue("start", start, Types.VARCHAR);
+		msps.addValue("end", end, Types.VARCHAR);
+		
+		return getNamedParameterJdbcTemplate().query(GET_SEARCH_BY_ID, msps,
+				new BeanPropertyRowMapper<VReview_AdminVo>(VReview_AdminVo.class));
 	}
 
 	@Override
 	public List<VReview_AdminVo> getReviews_by_title_n_content(String search, int start, int end) throws DataAccessException 
 	{
 		search = "%" + search + "%";
-		return getJdbcTemplate().query(GET_SEARCH_BY_TITLE_CONTENT, new Object[] { search, search },
+		
+		MapSqlParameterSource msps = new MapSqlParameterSource();
+		msps.addValue("search", search, Types.VARCHAR);
+		msps.addValue("start", start, Types.VARCHAR);
+		msps.addValue("end", end, Types.VARCHAR);
+		
+		return getNamedParameterJdbcTemplate().query(GET_SEARCH_BY_TITLE_CONTENT, msps,
 				new BeanPropertyRowMapper<VReview_AdminVo>(VReview_AdminVo.class));
 	}
 
--- trunk/_sora/Total_sora/src/naturalmom/data/dao/impl/VReview_FrontDaoOraImpl.java
+++ trunk/_sora/Total_sora/src/naturalmom/data/dao/impl/VReview_FrontDaoOraImpl.java
@@ -8,25 +8,33 @@ import naturalmom.data.model.VReview_FrontVo;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
-public class VReview_FrontDaoOraImpl extends JdbcDaoSupport implements IVReview_FrontDao 
+public class VReview_FrontDaoOraImpl extends NamedParameterJdbcDaoSupport implements IVReview_FrontDao 
 {
 	// 총 게시글 수 뽑기
 	private final String GET_COUNT_REVIEWS = "SELECT * FROM v_review_front WHERE product_no = ?";
 	// 게시판 목록 뽑기
-	private final String GET_ALL_LIST = "SELECT * FROM v_review_front WHERE product_no = ?";
+	private final String GET_ALL_LIST = "SELECT * FROM v_review_front WHERE product_no = :product_no AND ROWNUM >= :start AND ROWNUM <= :end";
 	
 	@Override
 	public List<VReview_FrontVo> getAllList(int product_no, int start, int end) throws DataAccessException
 	{
-		return getJdbcTemplate().query(GET_ALL_LIST, new BeanPropertyRowMapper<VReview_FrontVo>(VReview_FrontVo.class), new Integer(product_no));
+		MapSqlParameterSource msps = new MapSqlParameterSource();
+		msps.addValue("product_no", product_no, Types.INTEGER);
+		msps.addValue("start", start, Types.INTEGER);
+		msps.addValue("end", end, Types.INTEGER);
+	
+		return getNamedParameterJdbcTemplate().query(GET_ALL_LIST, msps, 
+				new BeanPropertyRowMapper<VReview_FrontVo>(VReview_FrontVo.class));
 	}
 
 	@Override
 	public int getCountReviews(int product_no) throws DataAccessException 
 	{
-		return getJdbcTemplate().queryForInt(GET_ALL_LIST, new Integer(product_no), Types.INTEGER);
+		return getJdbcTemplate().queryForInt(GET_COUNT_REVIEWS, new Integer(product_no), Types.INTEGER);
 	}
 	
 }
