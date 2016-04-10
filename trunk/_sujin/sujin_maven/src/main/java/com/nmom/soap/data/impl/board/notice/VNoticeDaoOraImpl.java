d715725db4dd2374ee1402cf32e9923334ec2020
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-10 13:54:48 +0000

    git-svn-id: https://dev.naver.com/svn/naturalmom@390 7e34a5f0-892d-0510-8adb-eb697a7bbe35

---
+++ trunk/_sujin/sujin_maven/src/main/java/com/nmom/soap/data/impl/board/notice/VNoticeDaoOraImpl.java
@@ -0,0 +1,116 @@
+package com.nmom.soap.data.impl.board.notice;
+
+import java.sql.Types;
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+
+import com.nmom.soap.data.dao.board.notice.IVNoticeDao;
+import com.nmom.soap.data.model.board.notice.VNoticeVo;
+
+public class VNoticeDaoOraImpl extends NamedParameterJdbcDaoSupport implements IVNoticeDao {
+	
+	final String GET_ALL_NOTICE = 
+			"SELECT notice_no,  ntc_title, ntc_re_no, ntc_write_day, ntc_hits, mem_id "
+			+ "FROM v_notice WHERE ROWNUM >= :start AND ROWNUM <= :end";
+	
+	final String GET_SEARCH_BY_TITLE_NOTICE = 
+			"SELECT notice_no,  ntc_title, ntc_re_no, ntc_write_day, ntc_hits, mem_id "
+			+ "FROM v_notice "
+			+ "WHERE ntc_title LIKE :search AND ROWNUM >= :start AND ROWNUM <= :end";
+//	"SELECT notice_no,  ntc_title, ntc_re_no, ntc_write_day, ntc_hits, mem_id FROM v_notice WHERE  ntc_title LIKE '%휴무%' AND ROWNUM >= :start AND ROWNUM <= :end";
+	final String GET_SEARCH_BY_CONTENT_NOTICE = 
+			"SELECT notice_no,  ntc_title, ntc_re_no, ntc_write_day, ntc_hits, mem_id FROM v_notice WHERE ntc_content LIKE :search AND ROWNUM >= :start AND ROWNUM <= :end";
+	//뷰에 콘텐트 없다.
+	final String GET_SEARCH_BY_ID_NOTICE = 
+			"SELECT notice_no,  ntc_title, ntc_re_no, ntc_write_day, ntc_hits, mem_id FROM v_notice WHERE mem_id LIKE :search AND ROWNUM >= :start AND ROWNUM <= :end";
+	
+	final String GET_SEARCH_BY_TITLE_N_CONTENT_NOTICE = 
+			"SELECT notice_no,  ntc_title, ntc_re_no, ntc_write_day, ntc_hits, mem_id FROM v_notice WHERE (ntc_title LIKE :search OR  ntc_content LIKE :search) AND ROWNUM >= :start AND ROWNUM <= :end";
+	
+	final String GET_ALL_COUNT = "SELECT COUNT(notice_no) FROM v_notice";
+	
+	final int LIMIT = 10; 
+	
+	public List<VNoticeVo> getAllNotice(int start, int end) throws DataAccessException {
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("start", new Integer(start), Types.INTEGER);
+		ps.addValue("end", new Integer(end), Types.INTEGER);
+		List<VNoticeVo> list = getNamedParameterJdbcTemplate().query(
+				GET_ALL_NOTICE, 
+				ps,
+				BeanPropertyRowMapper.newInstance(VNoticeVo.class));
+		if (list != null)
+			return list;
+		return null;
+	}
+
+	public List<VNoticeVo> getSearchByTitleNotice(String search, int start, int end) throws DataAccessException {
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("search", "%"+search+"%", Types.VARCHAR);
+		ps.addValue("start", new Integer(start), Types.INTEGER);
+		ps.addValue("end", new Integer(end), Types.INTEGER);
+		List<VNoticeVo> list = getNamedParameterJdbcTemplate().query(
+				GET_SEARCH_BY_TITLE_NOTICE, 
+				ps,
+				BeanPropertyRowMapper.newInstance(VNoticeVo.class));
+		if (list != null)
+			return list;
+		return null;
+	}
+
+	public List<VNoticeVo> getSearchByContentNotice(String search, int start, int end) throws DataAccessException {
+		// TODO Auto-generated method stub
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("search", "%"+search+"%", Types.VARCHAR);
+		ps.addValue("start", new Integer(start), Types.INTEGER);
+		ps.addValue("end", new Integer(end), Types.INTEGER);
+		List<VNoticeVo> list = getNamedParameterJdbcTemplate().query(
+				GET_SEARCH_BY_CONTENT_NOTICE,
+				ps,
+				BeanPropertyRowMapper.newInstance(VNoticeVo.class));
+		if (list != null)
+			return list;
+		return null;
+	}
+
+	public List<VNoticeVo> getSearchByIdNotice(String search, int start, int end) throws DataAccessException {
+		// TODO Auto-generated method stub
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("search", "%"+search+"%", Types.VARCHAR);
+		ps.addValue("start", new Integer(start), Types.INTEGER);
+		ps.addValue("end", new Integer(end), Types.INTEGER);
+		List<VNoticeVo> list = getNamedParameterJdbcTemplate().query(
+				GET_SEARCH_BY_ID_NOTICE,
+				ps,
+				BeanPropertyRowMapper.newInstance(VNoticeVo.class));
+		if (list != null)
+			return list;
+		return null;
+	}
+
+
+	public List<VNoticeVo> getSearchByTitleNContentNotice(String search, int start, int end) throws DataAccessException {
+		// TODO Auto-generated method stub
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("search", "%"+search+"%", Types.VARCHAR);
+		ps.addValue("start", new Integer(start), Types.INTEGER);
+		ps.addValue("end", new Integer(end), Types.INTEGER);
+		List<VNoticeVo> list = getNamedParameterJdbcTemplate().query(
+				GET_SEARCH_BY_TITLE_N_CONTENT_NOTICE,
+				ps,
+				BeanPropertyRowMapper.newInstance(VNoticeVo.class));
+		if (list != null)
+			return list;
+		return null;
+	}
+
+	public int getAllCount() throws DataAccessException {
+		// TODO Auto-generated method stub
+		return getJdbcTemplate().queryForInt(GET_ALL_COUNT);
+	}
+
+}
