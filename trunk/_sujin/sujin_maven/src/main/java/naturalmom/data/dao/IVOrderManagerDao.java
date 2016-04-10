ba12b58835543b6876b44399aa8696ef15458d45
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-10 13:03:17 +0000

    git-svn-id: https://dev.naver.com/svn/naturalmom@381 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/_sujin/sujin_maven/src/main/java/naturalmom/data/dao/IVOrderManagerDao.java
+++ trunk/_sujin/sujin_maven/src/main/java/naturalmom/data/dao/IVOrderManagerDao.java
@@ -9,6 +9,19 @@ import naturalmom.data.model.VOrderManagerVo;
 
 public interface IVOrderManagerDao {
 
-	public List<VOrderManagerVo> getAllOreder(int start, int end, int orderByNum)  throws DataAccessException;
+	public List<VOrderManagerVo> getAllOrederByDate(boolean orderBy)  throws DataAccessException;
+	
+	public List<VOrderManagerVo> getAllOrederByNo(boolean orderBy)  throws DataAccessException;
+	
+	public List<VOrderManagerVo> getAllOrederByName(boolean orderBy)  throws DataAccessException;
+	
+	public List<VOrderManagerVo> getAllOrederByProduct(boolean orderBy)  throws DataAccessException;
+	
+	public List<VOrderManagerVo> getAllOrederByNum(boolean orderBy)  throws DataAccessException;
+	
+	public List<VOrderManagerVo> getAllOrederByCharge(boolean orderBy)  throws DataAccessException;
+	
+	public List<VOrderManagerVo> getAllOrederByProcess(boolean orderBy)  throws DataAccessException;
+	
 	
 }
