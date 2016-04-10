e2f277b947b84fce54cfc11836f44402d4060991
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-10 13:54:13 +0000

    git-svn-id: https://dev.naver.com/svn/naturalmom@388 7e34a5f0-892d-0510-8adb-eb697a7bbe35

---
+++ trunk/_sujin/sujin_maven/src/main/java/com/nmom/soap/data/dao/order/IVOrderListDao.java
@@ -0,0 +1,14 @@
+package com.nmom.soap.data.dao.order;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.nmom.soap.data.model.order.VOrderListVo;
+
+
+public interface IVOrderListDao {
+
+	public List<VOrderListVo> getAllOreder(int start, int end, String mem_id) throws DataAccessException;
+	public int getAllCount(String mem_id) throws DataAccessException;
+}
