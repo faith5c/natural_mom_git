a73d441ca760244e7715d6919f1bf278b277f3d0
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-10 13:54:59 +0000

    git-svn-id: https://dev.naver.com/svn/naturalmom@391 7e34a5f0-892d-0510-8adb-eb697a7bbe35

---
+++ trunk/_sujin/sujin_maven/src/main/java/com/nmom/soap/data/impl/order/OrderDaoOraImpl.java
@@ -0,0 +1,94 @@
+package com.nmom.soap.data.impl.order;
+
+import java.sql.Types;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import com.nmom.soap.data.dao.order.IOrderDao;
+import com.nmom.soap.data.model.order.OrderVo;
+
+public class OrderDaoOraImpl extends NamedParameterJdbcDaoSupport implements IOrderDao {
+
+	
+	private SimpleJdbcInsert sji = null;
+	
+	final String ADD_ORDER = 
+			"INSERT INTO tb_order (charge, credit_num, credit_exp, rcv_name, rcv_phone, "
+			+ "rcv_addr_post, rcv_addr_detail, delivery_msg, credit_cd, delivery_com_cd, mem_id, order_no) "
+			+ "VALUES (:charge, :credit_num, :credit_exp, :rcv_name, :rcv_phone, :rcv_addr_post, :rcv_addr_detail,"
+			+ " :delivery_msg, (SELECT c.credit_cd FROM tb_credit c WHERE c.credit_nm = :credit_nm), "
+			+ "(SELECT d.delivery_com_cd FROM tb_delivery_com d WHERE d.delivery_com_nm = :delivery_com_nm),"
+			+ " :mem_id, ORDER_NO_SEQ.NEXTVAL)";
+	
+	final String EDIT_TRACKING_NUM_ORDER = 
+			"UPDATE NMDB.tb_order SET tracking_num = :tracking_num WHERE order_no = :order_no";
+	
+	
+/*	protected void initDao() throws Exception {
+		this.sji = new SimpleJdbcInsert(this.getDataSource());
+		this.sji.withTableName("tb_order");
+		this.sji.usingGeneratedKeyColumns("order_no"); //id <<PK, AI>> 
+		//스트링 배열로 여러개 받아오기 가능.. 정수 외에 문자도 유니크하면 받아오기 가능
+	}*/
+	
+	
+	public int addOrder(OrderVo order, String credit_nm, String delivery_com_nm) throws DataAccessException {
+		// TODO Auto-generated method stub
+		KeyHolder gkh = new GeneratedKeyHolder();
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("charge", new Integer(order.getCharge()), Types.INTEGER);
+		ps.addValue("credit_num", order.getCredit_num(), Types.VARCHAR);
+		ps.addValue("credit_exp", order.getCredit_exp(), Types.VARCHAR);
+		ps.addValue("rcv_name", order.getRcv_name(), Types.VARCHAR);
+		ps.addValue("rcv_phone", order.getRcv_phone(), Types.VARCHAR);
+		ps.addValue("rcv_addr_post", order.getRcv_addr_post(), Types.VARCHAR);
+		ps.addValue("rcv_addr_detail", order.getRcv_addr_detail(), Types.VARCHAR);
+		ps.addValue("delivery_msg", order.getDelivery_msg(), Types.VARCHAR);
+		ps.addValue("credit_nm", credit_nm, Types.VARCHAR);
+		ps.addValue("delivery_com_nm", delivery_com_nm, Types.VARCHAR);
+		ps.addValue("mem_id", order.getMem_id(), Types.VARCHAR);
+		this.getNamedParameterJdbcTemplate().update(ADD_ORDER, ps, gkh, new String[]{"order_no"});
+		
+		return gkh != null ? gkh.getKey().intValue() : 0;
+		
+				//( r > 0 && gkh != null) ? gkh.getKey().intValue() : -1;
+	}
+
+	
+	/*public int addOrder(OrderVo order, String credit_nm, String delivery_com_nm) throws DataAccessException {
+		// TODO Auto-generated method stub
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("charge", new Integer(order.getCharge()));
+		map.put("credit_num", order.getCredit_num());
+		map.put("credit_exp", order.getCredit_exp());
+		map.put("rcv_name", order.getRcv_name());
+		map.put("rcv_phone", order.getRcv_phone());
+		map.put("rcv_addr_post", order.getRcv_addr_post());
+		map.put("rcv_addr_detail", order.getRcv_addr_detail());
+		map.put("delivery_msg", order.getDelivery_msg());
+		map.put("tracking_num", new Integer(0));
+		map.put("credit_cd", 11);
+		map.put("delivery_com_cd", 11);
+		map.put("mem_id", order.getMem_id());
+		KeyHolder gkh = this.sji.executeAndReturnKeyHolder(map);
+//		Number num_key = this.sji.executeAndReturnKey(map);
+		System.out.println("SJI 방식으로 입력됨");
+//		return num_key != null ? num_key.intValue() : 0;
+		return gkh.getKey().intValue();
+				//( r > 0 && gkh != null) ? gkh.getKey().intValue() : -1;
+	}*/
+	
+	public int editTrackingNumOrder(int order_no, int tracking_num) throws DataAccessException {
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("order_no", new Integer(order_no), Types.INTEGER);
+		ps.addValue("tracking_num", new Integer(tracking_num), Types.INTEGER);
+		int r = this.getNamedParameterJdbcTemplate().update(EDIT_TRACKING_NUM_ORDER, ps);
+		return r;
+	}
+
+}
---
+++ trunk/_sujin/sujin_maven/src/main/java/com/nmom/soap/data/impl/order/VOrderListDaoOraImpl.java
@@ -0,0 +1,42 @@
+package com.nmom.soap.data.impl.order;
+
+import java.sql.Types;
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+
+import com.nmom.soap.data.dao.order.IVOrderListDao;
+import com.nmom.soap.data.model.order.VOrderListVo;
+
+public class VOrderListDaoOraImpl extends NamedParameterJdbcDaoSupport implements IVOrderListDao {
+	
+	final String GET_ALL_ORDER = "SELECT order_no, order_date, represent_img, product_name, buy_num, "
+			+ "charge, process_nm, mem_id FROM v_order_list WHERE ROWNUM >= :start AND ROWNUM <= :end AND mem_id = :mem_id";
+
+	final String GET_ALL_COUNT = "SELECT COUNT(order_no) FROM v_order_list WHERE mem_id = :mem_id";
+	
+	public List<VOrderListVo> getAllOreder(int start, int end, String mem_id) throws DataAccessException {
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("start", new Integer(start), Types.INTEGER);
+		ps.addValue("end", new Integer(end), Types.INTEGER);
+		ps.addValue("mem_id", mem_id, Types.VARCHAR);
+		List<VOrderListVo> list = getNamedParameterJdbcTemplate().query(
+				GET_ALL_ORDER, 
+				ps,
+				BeanPropertyRowMapper.newInstance(VOrderListVo.class));
+		if (list != null)
+			return list;
+		return null;
+	}
+
+	public int getAllCount(String mem_id) throws DataAccessException {
+		MapSqlParameterSource ps = new MapSqlParameterSource();
+		ps.addValue("mem_id", mem_id, Types.VARCHAR);
+		return getNamedParameterJdbcTemplate().queryForInt(GET_ALL_COUNT, ps);
+	}
+	
+
+}
