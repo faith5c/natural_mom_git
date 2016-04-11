c1b5c435e8b12d8aab21c829983a98a37001a27b
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-11 15:30:37 +0000

    관리자 주문관리 중 프로세스 코드 번호 다중 입력 구현
    git-svn-id: https://dev.naver.com/svn/naturalmom@439 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/data/dao/order/IProductOrderDao.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/data/dao/order/IProductOrderDao.java
@@ -12,8 +12,11 @@ public interface IProductOrderDao {
 	//	1. 삽입
 	public int addOrder(ProductOrderVo po) throws DataAccessException;
 	
-	//	2. 처리상태 변경(전체)
+	//	2. 처리상태 변경(하나)
 	public int editOrder(int order_no, int process_cd) throws DataAccessException;	
+	
+	//	2. 처리상태 변경(다수)
+	public int editOrder(int[] order_no, int process_cd) throws DataAccessException;	
 	
 	// 3. 처리상태 변경(일부)
 	public int editPartOrder(int order_no, String product_name, int process_cd) throws DataAccessException;
