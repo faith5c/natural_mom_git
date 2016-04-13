63a66a852cc80280d94cf2e0f3a5668f87eebd95
hook1028 <hook1028@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-13 10:50:51 +0000

    [소라] S 클래스에 static 변수 추가
    git-svn-id: https://dev.naver.com/svn/naturalmom@509 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/S.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/S.java
@@ -18,5 +18,18 @@ public class S {
 	final static int LEVEL_MEMBER = 1;
 	final static int LEVEL_BLACKLIST = 2;
 	final static int LEVEL_NONMEMBER = 3;
-
+	
+	// [소라] 수정, 삭제 시 실행 결과 값
+	final static int PROCESS_SUCCESS = 1;
+	final static int PROCESS_FAILURE = -1;
+	final static int PROCESS_NO_RESULT = 0;
+	
+	// [소라] 정렬 값
+	final static boolean ASC = true;
+	final static boolean DESC = false;
+	
+	// [소라] 진열, 판매, 삭제 상태 수정 시 true, false 값
+	public final static int STATE_TRUE = 1;
+	public final static int STATE_FALSE = 0;
+	
 }
