a4498aa2bdb7c3a1748cc77ed4320f9f93080363
hook1028 <hook1028@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-13 02:46:14 +0000

    [소라] sql문 수정
    git-svn-id: https://dev.naver.com/svn/naturalmom@462 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/data/impl/board/review/VReview_FrontDaoOraImpl.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/data/impl/board/review/VReview_FrontDaoOraImpl.java
@@ -4,18 +4,27 @@ import java.util.List;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.nmom.soap.data.dao.board.review.IVReview_FrontDao;
 import com.nmom.soap.data.model.board.review.VReview_FrontVo;
 
-public class VReview_FrontDaoOraImpl extends JdbcDaoSupport implements IVReview_FrontDao 
+public class VReview_FrontDaoOraImpl extends NamedParameterJdbcDaoSupport implements IVReview_FrontDao 
 {
 	// 게시판 목록 뽑기
-	private final String GET_ALL_LIST = "SELECT * FROM v_review_front WHERE product_no = ?";
+	private final String GET_ALL_LIST = "SELECT A.* FROM (SELECT rownum as re_rnum, V.* from V_REVIEW_FRONT V) A "
+			+ "WHERE A.product_no = :product_no AND A.re_rnum >= :start AND A.re_rnum <= :end ORDER BY re_rnum DESC;";
 	
-	public List<VReview_FrontVo> getAllList(int product_no) throws DataAccessException
+	public List<VReview_FrontVo> getAllList(int product_no, int start, int end) throws DataAccessException
 	{
-		return getJdbcTemplate().query(GET_ALL_LIST, new BeanPropertyRowMapper<VReview_FrontVo>(VReview_FrontVo.class), new Integer(product_no));
+		MapSqlParameterSource msps = new MapSqlParameterSource();
+		msps.addValue("product_no", new Integer(product_no));
+		msps.addValue("start", new Integer(start));
+		msps.addValue("end", new Integer(end));
+		
+		return getNamedParameterJdbcTemplate().query(GET_ALL_LIST, msps,
+				new BeanPropertyRowMapper<VReview_FrontVo>(VReview_FrontVo.class));
 	}
 }
