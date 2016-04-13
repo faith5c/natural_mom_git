47d183442f331287e38e74c868fe736e0c512bdf
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-13 06:46:30 +0000

    git-svn-id: https://dev.naver.com/svn/naturalmom@484 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/OrderController.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/OrderController.java
@@ -1,23 +1,30 @@
 package com.nmom.soap.controller;
 
-import com.nmom.soap.data.impl.order.OrderDaoOraImpl;
-import com.nmom.soap.data.impl.order.ProductOrderDaoOraImpl;
-import com.nmom.soap.data.impl.order.VOrderListDaoOraImpl;
+import com.nmom.soap.svc.order.IOrderSvc;
+import com.nmom.soap.svc.order.IProductOrderSvc;
+import com.nmom.soap.svc.order.IVOrderListSvc;
+import com.nmom.soap.svc.order.IVOrderManagerSvc;
 
 public class OrderController {
 
-	private OrderDaoOraImpl orderDaoOra;
-	private ProductOrderDaoOraImpl productOrderDaoOra;
-	private VOrderListDaoOraImpl vOrderListDaoOra;
+	private IOrderSvc orderSvc;
+	private IProductOrderSvc productOrderSvc;
+	private IVOrderListSvc vOrderListSvc;
+	private IVOrderManagerSvc vOrderManagerSvc;
 	
 	
-	public void setOrderDaoOra(OrderDaoOraImpl orderDaoOra) {
-		this.orderDaoOra = orderDaoOra;
+	
+	public void setOrderSvc(IOrderSvc orderSvc) {
+		this.orderSvc = orderSvc;
 	}
-	public void setProductOrderDaoOra(ProductOrderDaoOraImpl productOrderDaoOra) {
-		this.productOrderDaoOra = productOrderDaoOra;
+	public void setProductOrderSvc(IProductOrderSvc productOrderSvc) {
+		this.productOrderSvc = productOrderSvc;
 	}
-	public void setVOrderListDaoOra(VOrderListDaoOraImpl vOrderListDaoOra) {
-		this.vOrderListDaoOra = vOrderListDaoOra;
+	public void setVOrderListSvc(IVOrderListSvc vOrderListSvc) {
+		this.vOrderListSvc = vOrderListSvc;
 	}
+	public void setVOrderManagerSvc(IVOrderManagerSvc vOrderManagerSvc) {
+		this.vOrderManagerSvc = vOrderManagerSvc;
+	}
+	
 }
