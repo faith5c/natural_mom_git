49549f86162a62dd6eaa7cf2603f0af394d5fd16
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-13 06:59:51 +0000

    세일 컨트롤러 수정(세일 서비스 추가)
    git-svn-id: https://dev.naver.com/svn/naturalmom@493 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/SalesController.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/SalesController.java
@@ -1,13 +1,20 @@
 package com.nmom.soap.controller;
 
+import com.nmom.soap.svc.sales.ISaleStatementSvc;
 import com.nmom.soap.svc.sales.IVRank_SellsSvc;
 
 public class SalesController 
 {
-	IVRank_SellsSvc rank_sellsSvc;
+	private IVRank_SellsSvc rank_sellsSvc;
+	private ISaleStatementSvc saleStatementSvc;
 	
 	public void setRank_sellsSvc(IVRank_SellsSvc rank_sellsSvc) 
 	{
 		this.rank_sellsSvc = rank_sellsSvc;
+	}
+	
+	
+	public void setSaleStatementSvc(ISaleStatementSvc saleStatementSvc) {
+		this.saleStatementSvc = saleStatementSvc;
 	}
 }
