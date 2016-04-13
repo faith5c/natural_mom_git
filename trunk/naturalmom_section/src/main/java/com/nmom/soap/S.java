84522d49485933739fd9aa316336a75f8cb22f02
hook1028 <hook1028@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-13 10:53:09 +0000

    [소라] S 클래스에 static 변수 수정
    git-svn-id: https://dev.naver.com/svn/naturalmom@510 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/S.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/S.java
@@ -3,30 +3,30 @@ package com.nmom.soap;
 public class S {
 	
 	// 게시판 번호
-	final static int BOARD_NOTICE =1;
-	final static int BOARD_EVENT =2;
-	final static int BOARD_FAQ =3;
-	final static int BOARD_QNA =4;
-	final static int BOARD_REVIEW =5;
+	public final static int BOARD_NOTICE =1;
+	public final static int BOARD_EVENT =2;
+	public final static int BOARD_FAQ =3;
+	public final static int BOARD_QNA =4;
+	public final static int BOARD_REVIEW =5;
 	
 	// 성별
-	final static int GENDER_MALE =1;
-	final static int GENDER_FEMALE =2;
+	public final static int GENDER_MALE =1;
+	public final static int GENDER_FEMALE =2;
 	
 	// 회원등급
-	final static int LEVEL_ADMIN = 10;
-	final static int LEVEL_MEMBER = 1;
-	final static int LEVEL_BLACKLIST = 2;
-	final static int LEVEL_NONMEMBER = 3;
+	public final static int LEVEL_ADMIN = 10;
+	public final static int LEVEL_MEMBER = 1;
+	public final static int LEVEL_BLACKLIST = 2;
+	public final static int LEVEL_NONMEMBER = 3;
 	
 	// [소라] 수정, 삭제 시 실행 결과 값
-	final static int PROCESS_SUCCESS = 1;
-	final static int PROCESS_FAILURE = -1;
-	final static int PROCESS_NO_RESULT = 0;
+	public final static int PROCESS_SUCCESS = 1;
+	public final static int PROCESS_FAILURE = -1;
+	public final static int PROCESS_NO_RESULT = 0;
 	
 	// [소라] 정렬 값
-	final static boolean ASC = true;
-	final static boolean DESC = false;
+	public final static boolean ASC = true;
+	public final static boolean DESC = false;
 	
 	// [소라] 진열, 판매, 삭제 상태 수정 시 true, false 값
 	public final static int STATE_TRUE = 1;
