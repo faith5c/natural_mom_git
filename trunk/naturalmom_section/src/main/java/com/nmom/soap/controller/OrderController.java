c17ecbf5b0b0345c6efba5b45f07f74c59cc73c9
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-13 05:15:25 +0000

    오더 컨트롤러 추가
    git-svn-id: https://dev.naver.com/svn/naturalmom@474 7e34a5f0-892d-0510-8adb-eb697a7bbe35

---
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.nmom.soap.controller;
+
+import com.nmom.soap.data.impl.order.OrderDaoOraImpl;
+import com.nmom.soap.data.impl.order.ProductOrderDaoOraImpl;
+import com.nmom.soap.data.impl.order.VOrderListDaoOraImpl;
+
+public class OrderController {
+
+	private OrderDaoOraImpl orderDaoOra;
+	private ProductOrderDaoOraImpl productOrderDaoOra;
+	private VOrderListDaoOraImpl vOrderListDaoOra;
+	
+	
+	public void setOrderDaoOra(OrderDaoOraImpl orderDaoOra) {
+		this.orderDaoOra = orderDaoOra;
+	}
+	public void setProductOrderDaoOra(ProductOrderDaoOraImpl productOrderDaoOra) {
+		this.productOrderDaoOra = productOrderDaoOra;
+	}
+	public void setvOrderListDaoOra(VOrderListDaoOraImpl vOrderListDaoOra) {
+		this.vOrderListDaoOra = vOrderListDaoOra;
+	}
+	
+}
