c3273dbbcd126cbc9dde206617dab120fe5761c6
faith5c <faith5c@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-13 13:01:49 +0000

    [수민] @Controller 붙임
    git-svn-id: https://dev.naver.com/svn/naturalmom@527 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/CartController.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/CartController.java
@@ -1,8 +1,11 @@
 package com.nmom.soap.controller;
 
+import org.springframework.stereotype.Controller;
+
 import com.nmom.soap.svc.cart.ICartSvc;
 import com.nmom.soap.svc.cart.IVCartProductSvc;
 
+@Controller
 public class CartController {
 	
 	ICartSvc cartSvc;
@@ -14,4 +17,18 @@ public class CartController {
 	public void setVCartProductSvc(IVCartProductSvc vCartProductSvc) {
 		this.vCartProductSvc = vCartProductSvc;
 	}
+	/*
+	@RequestMapping(value ="/cart.nm", method=RequestMethod.GET)
+	public ModelAndView menu_cart(HttpServletRequest req, HttpSession ses)
+	{
+		String loggedin = (String)ses.getAttribute("loggedin");
+		
+		List<VCartProductVo> cart_list = this.vCartProductSvc.getCartByMemId(loggedin);
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("cart_list", cart_list);
+		
+		ModelAndView mav = new ModelAndView("order/cart");
+		return "order/cart";
+	}*/
 }
