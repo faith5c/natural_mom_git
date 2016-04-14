a18af557106ae7b99925b9fda1be0d9494079ff0
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-14 14:34:34 +0000

    노티스콘트롤러에 공지사항 리스트 추가
    git-svn-id: https://dev.naver.com/svn/naturalmom@591 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/NoticeController.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/NoticeController.java
@@ -1,7 +1,19 @@
 package com.nmom.soap.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.nmom.soap.S;
+import com.nmom.soap.data.model.board.notice.VNoticeVo;
 import com.nmom.soap.svc.board.notice.INoticeReSvc;
 import com.nmom.soap.svc.board.notice.INoticeSvc;
 import com.nmom.soap.svc.board.notice.IVNoticeSvc;
@@ -23,5 +35,71 @@ public class NoticeController {
 		this.vNoticeSvc = vNoticeSvc;
 	}
 	
+	//공지사항 리스트(회원)
+	@RequestMapping (value="/board/notice.nm", method=RequestMethod.GET)
+	public ModelAndView getAllNotice(HttpServletRequest req,
+			@RequestParam(value="ab", required=false) String allBlock,
+			@RequestParam(value="nb", required=false) String nowBlock,
+			@RequestParam(value="k", required=false) String kind,
+			@RequestParam(value="s", required=false) String search){
+		
+		//블럭은 페이지 10개씩의 단위 첫페이지는 무조건 1
+		int block = 1;
+		
+		//블럭 갯수
+		int blockNums = 0;
+		
+		//전체 페이지 수
+		int allPages = 0;
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		if( nowBlock != null && !nowBlock.isEmpty() && !nowBlock.equals("")){
+			try{
+				block = Integer.parseInt(nowBlock);
+			}catch(NumberFormatException ne){
+				ne.printStackTrace();
+			}
+			
+		}
+		if( search == null || search.isEmpty() || search.equals("") ){
+			allPages = vNoticeSvc.getAllCount();
+			
+			List<VNoticeVo> list = this.vNoticeSvc.getAllNotice(block, allPages);
+			if(allPages > 0)
+			blockNums = (int)Math.ceil((double)allPages/S.PAGE_LIMIT);
+		
+			map.put("ab", blockNums);
+			map.put("nb", block);
+			map.put("no_list", list);
+			return new ModelAndView("/board/b_notice", map);
+		}
+		
+		return null;
+	}
+	
+	
+	
+	//공지사항 글 상세보기(회원)
+	
+	//공지사항 댓글 달기(회원)
+	
+	//공지사항 댓글 수정(회원)
+	
+	//공지사항 댓글 삭제(회원)
+	
+	//공지사항 리스트(어드민)
+	
+	//공지사항 글 상세보기(어드민)
+	
+	//공지사항 글 추가하기(어드민)
+	
+	//공지사항 글 수정하기(어드민)
+	
+	//공지사항 댓글 달기(어드민)
+	
+	//공지사항 댓글 수정(어드민)
+	
+	//공지사항 댓글 삭제(어드민)
 	
 }
