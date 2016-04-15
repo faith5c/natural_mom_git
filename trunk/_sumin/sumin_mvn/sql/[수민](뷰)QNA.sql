9085d3a70d6577b90803c3b0cd1431fc8655b04e
faith5c <faith5c@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-15 10:45:42 +0000

    [수민] qna 뷰 sql 변경
    git-svn-id: https://dev.naver.com/svn/naturalmom@627 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/_sumin/sumin_mvn/sql/[수민](뷰)QNA.sql
+++ trunk/_sumin/sumin_mvn/sql/[수민](뷰)QNA.sql
@@ -4,8 +4,8 @@
 -- (html : 댓글 갯수가 0이 아니라면 표시해줌)
 
 
--- 뷰 만듦
-CREATE VIEW v_qna_qnare 
+-- @@@@ 뷰 만듦
+CREATE OR REPLACE VIEW v_qna_qnare
 AS
 SELECT q.qna_no, qna_title, qna_write_day, qna_hits, qna_content, qna_pw, qna_pos, qna_ref, q.mem_id, 
 NVL((SELECT COUNT(qna_re_no) FROM tb_qna_re r WHERE r.qna_no=q.qna_no AND r.qna_re_del_check=0), 0) as qna_re_count 
@@ -13,27 +13,25 @@ FROM tb_qna q LEFT OUTER JOIN tb_qna_re r
 ON (q.qna_no = r.qna_no)
 WHERE qna_del_check=0
 GROUP BY q.qna_no, qna_title, qna_write_day, qna_hits, qna_content, qna_pw, qna_pos, qna_ref, q.mem_id
-ORDER BY qna_ref ASC, qna_pos ASC;
+ORDER BY qna_ref ASC, qna_pos DESC;
 
 -- 글하나 조회 (qna_no로 찾음)
 SELECT qna_no, qna_title, qna_write_day, qna_hits, qna_content, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count
 FROM v_qna_qnare WHERE qna_no=1;
 
 
--- 모든 글 조회
+-- 모든 글 조회 (범위 지정 못함)
 SELECT qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
 FROM v_qna_qnare
 GROUP BY qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
 ORDER BY qna_ref DESC, qna_pos DESC;
 
-
+--@@@ 모든 글 조회 (범위 지정 가능, row_num 이용)
 SELECT A.* FROM 
   (SELECT rownum as qna_rnum, X.* FROM
     (
     SELECT qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
     FROM v_qna_qnare
-    GROUP BY qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
-    ORDER BY qna_ref ASC, qna_pos ASC
     ) X 
   WHERE rownum <= 5) A 
 WHERE A.qna_rnum >= 1 ORDER BY A.qna_rnum DESC;
@@ -48,15 +46,13 @@ WHERE qna_title LIKE '%답변%'
 GROUP BY qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
 ORDER BY qna_ref DESC, qna_pos DESC;
 
-
+-- @@ 검색(제목)
 SELECT A.* FROM 
   (SELECT rownum as qna_rnum, X.* FROM
     (
     SELECT qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
     FROM v_qna_qnare
     WHERE qna_title LIKE '%답변%'
-    GROUP BY qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
-    ORDER BY qna_ref ASC, qna_pos ASC
     ) X 
   WHERE rownum <= 5) A 
 WHERE A.qna_rnum >= 1 ORDER BY A.qna_rnum DESC;
@@ -69,15 +65,13 @@ WHERE qna_content LIKE '%답변%'
 GROUP BY qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
 ORDER BY qna_ref DESC, qna_pos DESC;
 
-
+--@@ 검색(내용)
 SELECT A.* FROM 
   (SELECT rownum as qna_rnum, X.* FROM
     (
     SELECT qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
     FROM v_qna_qnare
     WHERE qna_content LIKE '%답변%'
-    GROUP BY qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
-    ORDER BY qna_ref ASC, qna_pos ASC
     ) X 
   WHERE rownum <= 5) A 
 WHERE A.qna_rnum >= 1 ORDER BY A.qna_rnum DESC;
@@ -89,21 +83,19 @@ WHERE (qna_title LIKE '%답변%' OR qna_content LIKE '%답변%')
 GROUP BY qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
 ORDER BY qna_ref DESC, qna_pos DESC;
 
-
+--@@ 검색(제목+내용)
 SELECT A.* FROM 
   (SELECT rownum as qna_rnum, X.* FROM
     (
     SELECT qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
     FROM v_qna_qnare
     WHERE (qna_title LIKE '%답변%' OR qna_content LIKE '%답변%')
-    GROUP BY qna_no, qna_title, qna_write_day, qna_hits, qna_pw, qna_pos, qna_ref, mem_id, qna_re_count  
-    ORDER BY qna_ref ASC, qna_pos ASC
     ) X 
   WHERE rownum <= 5) A 
 WHERE A.qna_rnum >= 1 ORDER BY A.qna_rnum DESC;
 
 
 -------------------------------------------------------------
--- 쉽게 하는 ruwnum
+-- 쉽게 하는 ruwnum  <- 사용하면 안됨!
 SELECT A.* FROM (SELECT rownum as re_rnum, V.* from V_REVIEW_ADMIN V) A 
 WHERE A.re_rnum >= 2 AND A.re_rnum <= 4;
