8444d28a87eab2842683776a66f5aed4cdf6569b
hook1028 <hook1028@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-16 17:03:38 +0000

    [소라] product 중간 백업
    git-svn-id: https://dev.naver.com/svn/naturalmom@662 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/data/dao/category/ICategoryDao.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/data/dao/category/ICategoryDao.java
@@ -1,9 +1,16 @@
 package com.nmom.soap.data.dao.category;
 
+import java.util.List;
+
 import org.springframework.dao.DataAccessException;
+
+import com.nmom.soap.data.model.category.CategoryVo;
 
 public interface ICategoryDao {
 	
 	//카테고리코드로 카테고리이름가져옴
 	String getCategoryNameByCd(int category_cd) throws DataAccessException;
+
+	// 모든 카테고리 정보 가져오기
+	List<CategoryVo> getAllCategory();
 }
