d6978e64c9e4427638315f3e8fb7ec9ef7f35d10
hook1028 <hook1028@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-16 17:41:26 +0000

    [소라] product 중간 백업
    git-svn-id: https://dev.naver.com/svn/naturalmom@666 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/ProductController.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/controller/ProductController.java
@@ -1,15 +1,19 @@
 package com.nmom.soap.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.nmom.soap.S;
@@ -51,7 +55,7 @@ public class ProductController
 	
 	// 상품관리 페이지
 	@RequestMapping(value ="/admin/product.nm", method=RequestMethod.GET)
-	public ModelAndView product_manageList(HttpServletRequest request,
+	public ModelAndView product_manageList(HttpServletRequest request, HttpSession session,
 			@RequestParam(value="by", required=false) String by, @RequestParam(value="order", required=false) String order)
 	{		
 		List<VProduct_ManageVo> list = null;
@@ -127,7 +131,7 @@ public class ProductController
 	
 	// 삭제된 상품관리
 	@RequestMapping(value="/admin/product.nm", method=RequestMethod.GET, params="page=deleted")
-	public ModelAndView product_deletedList(HttpServletRequest request,
+	public ModelAndView product_deletedList(HttpServletRequest request, HttpSession session,
 			@RequestParam(value="by", required=false) String by, @RequestParam(value="order", required=false) String order)
 	{
 		List<VProduct_DeletedVo> list = null;
@@ -188,7 +192,7 @@ public class ProductController
 	
 	// 상품관리 & 상품삭제 페이지에서 상태 변경
 	@RequestMapping(value ="/admin/product.nm", method=RequestMethod.GET, params="page=process")
-	public String product_changeState(HttpServletRequest request,
+	public String product_changeState(HttpServletRequest request, HttpSession session,
 			@RequestParam(value="item", required=false) String item, @RequestParam(value="order", required=false) int order,
 			@RequestParam(value="no", required=false) int[] no)
 	{
@@ -239,13 +243,66 @@ public class ProductController
 
 	// 상품 등록 페이지
 	@RequestMapping(value ="/admin/product.nm", method=RequestMethod.GET, params="page=register")
-	public ModelAndView product_register (HttpServletRequest request)
+	public ModelAndView product_prepareRegister (HttpServletRequest request, HttpSession session)
 	{
 		List<CategoryVo> c_list = categorySvc.getAllCategory();
 		Map<String, Object> map = new HashMap<String, Object>();
 		
 		map.put("c_list", c_list);
 		return new ModelAndView("admin/product/a_product", map);
+	}
+	
+	@RequestMapping(value ="/admin/product_reg_proc.nm", method=RequestMethod.POST)
+	public ModelAndView product_register(HttpServletRequest request, HttpSession session,
+			@RequestParam (value="represent_img") MultipartFile represent_img,
+			@RequestParam (value="detail_img") MultipartFile detail_img,
+			@RequestParam Map<String, String> pro_param) throws IllegalStateException, IOException
+	{
+		//***************************************** ******************************* 업로드 파일 처리 부분
+		// 이미지 업로드 경로 지정
+		String root_path = request.getSession().getServletContext().getRealPath("/");  
+	    String attach_path = "resources/product_images/";
+	    // 이미지 파일 이름 가져옴
+	    String re_imgName = represent_img.getOriginalFilename();
+	    String de_imgName = detail_img.getOriginalFilename();
+	    
+	    File re_img = new File(root_path + attach_path + re_imgName);
+	    File de_img = new File(root_path + attach_path + de_imgName);
+	    
+	    represent_img.transferTo(re_img);
+	    detail_img.transferTo(de_img);
+	    
+//	    // 예외 없이 정상 물리 파일 저장이 완료 되었다면?
+//	 	long realFilesize1 = re_img.length();
+//	 	String realFileName1 = re_img.getName();
+//	 	String realFilePath1 = re_img.getAbsolutePath();
+//	 	System.out.println("FILE NAME: " + realFileName1);
+//	 	System.out.println("FILE PATH: " + realFilePath1);
+//	 	System.out.println("FILE SIZE: " + realFilesize1);
+//	 	
+//	 	long realFilesize2 = de_img.length();
+//	 	String realFileName2 = de_img.getName();
+//	 	String realFilePath2 = de_img.getAbsolutePath();
+//	 	System.out.println("FILE NAME: " + realFileName2);
+//	 	System.out.println("FILE PATH: " + realFilePath2);
+//	 	System.out.println("FILE SIZE: " + realFilesize2);
+	  //***************************************** ******************************* 업로드 파일 처리 부분
+	    // 파일 등록 부분
+	    int category_cd = 0;
+
+	    try
+	    { 
+	    	category_cd = Integer.parseInt(pro_param.get("category_cd"));
+	    }
+	    catch(NumberFormatException e)
+	    {
+	    	// 에러 페이지 이동
+	    }
+	    
+	    ProductVo new_product = new ProductVo();
+//	    new_product.setCategory_cd());
+		
+	    return new ModelAndView("admin/product/a_product", null);
 	}
 	
 	//*********************************************************************//
