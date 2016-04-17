6b7e44ffcbced48ca379d3bbaffc84ce6d023ecd
nata1116 <nata1116@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-17 11:28:38 +0000

    [수진] 다오 이름 변경...
    git-svn-id: https://dev.naver.com/svn/naturalmom@689 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/naturalmom_section/src/main/java/com/nmom/soap/svc/member/VOrdererSvcImpl.java
+++ trunk/naturalmom_section/src/main/java/com/nmom/soap/svc/member/VOrdererSvcImpl.java
@@ -6,21 +6,17 @@ import com.nmom.soap.data.model.member.VOrdererVo;
 
 public class VOrdererSvcImpl implements IVOrdererSvc {
 
-	private IVOrdererDao vOrdererDao;
-	
-	
-	
-	public void setVOrdererDao(IVOrdererDao vOrdererDao) {
-		vOrdererDao = vOrdererDao;
+	private IVOrdererDao ordererDao;
+
+	public void setOrdererDao(IVOrdererDao ordererDao) {
+		this.ordererDao = ordererDao;
 	}
-
-
 
 	@Override
 	public VOrdererVo getOrderer(String id) {
 		System.out.println("오더러 서비스");
-		System.out.println(vOrdererDao == null ? null : vOrdererDao);
-		return this.vOrdererDao.getOrderer(id);
+		System.out.println(ordererDao == null ? null : ordererDao);
+		return this.ordererDao.getOrderer(id);
 	}
 
 }
