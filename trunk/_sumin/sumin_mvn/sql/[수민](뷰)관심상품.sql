c8fbe2d565a26e58fc43140dc97687384d88010e
faith5c <faith5c@7e34a5f0-892d-0510-8adb-eb697a7bbe35>
2016-04-19 08:19:08 +0000

    [수민] 관심상품/장바구니 뷰 수정
    git-svn-id: https://dev.naver.com/svn/naturalmom@743 7e34a5f0-892d-0510-8adb-eb697a7bbe35

--- trunk/_sumin/sumin_mvn/sql/[수민](뷰)관심상품.sql
+++ trunk/_sumin/sumin_mvn/sql/[수민](뷰)관심상품.sql
@@ -1,12 +1,12 @@
 
 -- 관심상품 뷰 만듦
-CREATE VIEW V_INTEREST_PRODUCT
+CREATE OR REPLACE VIEW V_INTEREST_PRODUCT
 AS
-SELECT i.product_no, product_name, selling_price, sale_state, represent_img, i.mem_id
+SELECT i.product_no, product_name, selling_price, sale_state, represent_img, stock, i.mem_id
 FROM tb_product p JOIN tb_interest i
 ON (p.product_no = i.product_no);
 
 -- 만들어진 관심상품 뷰에 적용되어야 할 SQL
-SELECT product_no, product_name, selling_price, sale_state, represent_img, mem_id
+SELECT product_no, product_name, selling_price, sale_state, represent_img, stock, mem_id
 FROM V_INTEREST_PRODUCT
 WHERE mem_id='soomin';
